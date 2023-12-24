25203c31cb9bda6dc4a274a284ab18682ba85b4c
Chetan51423 <chetanshinde51423@gamil.com>
2023-12-24 19:22:10 +0530

    add the JSX expression for creating H1 element using JSX

--- Index.js
+++ Index.js
@@ -1,9 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const heading1 = React.createElement('h1', {id:'title'}, 'Root-> container-> h1 I have Ignited the react app');
-const heading2 = React.createElement('h1',{id:'title2'}, 'Root-> container-> h2');
-const div2 = React.createElement('div', {id:'container'}, [heading1, heading2]);
+// # creatingelements in react using React.createElement()
+
+// const heading1 = React.createElement('h1', {id:'title'}, 'Root-> container-> h1 I have Ignited the react app');
+// const heading2 = React.createElement('h1',{id:'title2'}, 'Root-> container-> h2');
+// const div2 = React.createElement('div', {id:'container'}, [heading1, heading2]);
+
+
+//Creating elemets using JSX
+const heading = (<h1 id="container">Hello from JSX and babel</h1>)    // called as JSX Expression
 
 const root2 = ReactDOM.createRoot(document.getElementById('root'));
-root2.render(div2);
+root2.render(heading);
