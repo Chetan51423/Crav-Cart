2e941ccbab958ffc39adb5935e37736fa4b7fd7c
Chetan51423 <chetanshinde51423@gamil.com>
2024-02-25 17:05:52 +0530

    one was giving an error while building the app in netlify so solved the issue

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,11 +1,12 @@
-import Happy_aunty from "../Asets/Happy_aunty.jpg"
+
 const About = ()=>
 {
     return (
             <div className="mx-16 my-auto p-12 bg-orange-300 rounded-2xl shadow-2xl">
                 <div className="flex  flex-col justify-center items-center">
                     <div className="w-40 h-40">
-                        <img className="rounded-3xl" src={Happy_aunty} alt="logo" />
+                        
+                        <img className="rounded-3xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoGndx19Dehe1qY2ZDX0Q2zFMRxb5QjOp4H5vIV30gPkq7ECVPwFJyElPSZpGNdA-fn-w&usqp=CAU" alt="logo" />
                     </div>
                     <h1 className="text-4xl font-medium">Aunty Didn't Like To Share Anything About Her Due To Privacy Reasons</h1>
                 </div>
